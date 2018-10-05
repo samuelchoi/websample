03969a6c228c11b98d8e768c9756a300147e1e56
samuelchoi <samuelchoi@163.com>
2018-10-05 18:44:03 +0800

    first commit

---
+++ src/main/java/com/snow/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.snow.controller;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class UserController extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        doGet(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        response.setContentType("test/html");
+
+        System.out.print("into servlet .");
+        PrintWriter out = response.getWriter();
+        out.println("This is User Servlet .");
+    }
+
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello World4!</h2>
+</body>
+</html>
